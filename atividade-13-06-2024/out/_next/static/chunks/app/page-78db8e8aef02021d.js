dd97f4311b81d218e95f779c3c99a6ed6fa7fc7f
Renan Geraldini Leão <131619149+renanjava@users.noreply.github.com>
2024-06-16 21:35:48 -0300

    build pt.4

---
+++ atividade-13-06-2024/out/_next/static/chunks/app/page-78db8e8aef02021d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{6921:function(n,e,u){Promise.resolve().then(u.t.bind(u,895,23))},895:function(){}},function(n){n.O(0,[239,971,23,744],function(){return n(n.s=6921)}),_N_E=n.O()}]);
